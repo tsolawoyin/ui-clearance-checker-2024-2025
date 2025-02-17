936a1c3c899ede0c2b9f47d8fec336c3e1312d09
tsolawoyin <t.olawoyin@outlook.com>
2025-02-17 04:38:22 +0100

    Init commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Form from "./Form";
+
+import "./css/App.css";
+
+function Header() {
+    return (
+        <h3 className="has-text-centered">UI clearance status checker 2024/2025</h3>
+    )
+}
+
+function Info() {
+    return (
+        <div className="note">
+            <p><u>Note</u></p>
+            {/* <p>You can verify manually <a href="https://ui.edu.ng/news/candidates-20242025-academic-session-yet-be-cleared-admissions-office-undergraduates">here</a></p> */}
+            <p>This web app is <strong>unofficial</strong>. It is not owned by <em>UI</em>.</p>
+            <p>It is created by <a href="https://github.com/tsolawoyin">tsolawoyin</a>, also a UI student, to help others check their cleareance status easily.</p>
+            <p>The application works by checking the list of uncleared candidates provided <a href="https://ui.edu.ng/news/candidates-20242025-academic-session-yet-be-cleared-admissions-office-undergraduates">here</a> on the official UI page. If your Reg num is found, <em><strong>you are not cleared yet</strong></em>. And if not, the app assumes that <em><strong>you have probably been cleared.</strong></em></p>
+            <p>This app is made for convenience and fast look up. You can still verify manually <a href="https://ui.edu.ng/news/candidates-20242025-academic-session-yet-be-cleared-admissions-office-undergraduates">here</a></p>
+            <p>This program is open source. <em>MIT License</em>. Code can be found <a href="">here</a></p>
+            <p>You can also check out my web app, <a href="https://boots5.netlify.app"><em>Boots</em></a>, highly recommended for those who are writing UTME.</p>
+        </div>
+    )
+}
+
+function App() {
+
+    return (
+        <div className="app">
+            <div className="div">
+                <Header />
+                <Form />
+                <Info />
+            </div>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/Form.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+import { notCleared } from "./notCleared";
+
+function Form() {
+    let [regNum, setRegNum] = useState("");
+    let [successInfo, setSuccessInfo] = useState("");
+    let [info, setInfo] = useState("");
+    let [errInfo, setErrInfo] = useState("")
+    let regex = /^(\d+)?(2024\d{8}[a-zA-Z]{2})$/
+
+    function validate(regNum, main) {
+        if (main.includes(regNum)) {
+            let x = main.match(regex);
+            let line = x[1];
+            setErrInfo(`Oops you haven't be cleared. Your reg num is found at line ${line}`)
+            setInfo("");
+            setSuccessInfo("");
+            return true;
+        }
+
+        return false;
+    }
+
+    function handleSubmit(ev) {
+        ev.preventDefault();
+
+        let list = notCleared.split("\n");
+
+        if (regex.test(regNum)) {
+            let jambReg = list.find(r => validate(regNum, r))
+            if (!jambReg) {
+                // if it is false at this point, then user have been cleared.
+                setSuccessInfo("You have probably been cleared.");
+                setInfo("");
+                setErrInfo(""); // set err inf to nothing
+            }
+        } else {
+            setInfo("Invalid reg number! Check again");
+            setSuccessInfo("");
+            setErrInfo("");
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="form">
+            <label htmlFor="">
+                <input type="text" className="input" placeholder="enter your jamb reg number..." value={regNum} onChange={(ev) => {
+                    setRegNum(ev.target.value);
+                }} />
+            </label>
+            <button className="button" style={{
+                marginTop: ".5em"
+            }}>Check</button>
+            {successInfo && <p className="has-text-success">{successInfo}. Check <a href="https://admissions.ui.edu.ng/#/applicant/applicant-dashboard">here</a> regularly to know the status of your clearance.</p>}
+            {
+                info && <p className="has-text-warning">{info}</p>
+            }
+            {errInfo && <p className="has-text-danger">{errInfo}. Click <a href="https://ui.edu.ng/news/candidates-20242025-academic-session-yet-be-cleared-admissions-office-undergraduates">here</a> for more information.</p>}
+        </form>
+    )
+}
+
+export default Form;
